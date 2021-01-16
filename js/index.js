105913556eb31f25472f83d1778488b61827cc26
Abram-green <54475167+Abram-green@users.noreply.github.com>
2021-01-16 14:45:17 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -21,8 +21,7 @@ $(function() {
         event.preventDefault();
        
         $('html, body').animate({
-          scrollTop: $($(this).attr('href')).offset().top,
-          scrollBottom: $($(this).attr('href')).offset().top
+          scrollTop: $($(this).attr('href')).offset().top
         }, 500, 'linear');
 
         $('.lists-item a').removeClass('active');
